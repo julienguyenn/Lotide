10e93da03cf9ea33e38ddde23614605fd5e7644b
Julie Nguyen <julie-nguyen@live.com>
2019-11-26 21:23:50 +0000

    created function to flatten a nested loop

---
+++ flatten.js
@@ -0,0 +1,40 @@
+const eqArrays = function(array1, array2) {
+  if (array1 === undefined ||
+    array2 === undefined ||
+    array1.length  !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === false) {
+    console.log("👻These two arrays are not the same.");
+  } else {
+    console.log("🤗These arrays are the same.");
+  }
+};
+
+const flatten = function(array) {
+  let newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      let nested = array[i];
+      for (let index = 0; index < nested.length; index++) {
+        newArr.push(nested[index]);
+      }
+    } else {
+      newArr.push(array[i]);
+    }
+  }
+  console.log(newArr);
+  return newArr;
+}
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2 , 3, 4, 5, 6]);
+
