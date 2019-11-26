e85018d05532ec5a92d840507d1e96249efd001e
Julie Nguyen <julie-nguyen@live.com>
2019-11-26 20:49:17 +0000

    modified slightly

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('😊🤗 Assertion Passed: [' + actual + '] === [' + expected + ']');
+    console.log(`😊🤗 Assertion Passed: [${actual}] === [${expected}]`);
   } else {
-    console.log('😤😢 Assertion Failed: [' + actual + '] !== [' + expected + ']');
+    console.log(`😤😢 Assertion Failed: [${actual}] !== [${expected}]`);
   }
 };
 
