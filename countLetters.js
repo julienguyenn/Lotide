5eea33f6260a371cc6913597e700ab91450eeb04
Julie Nguyen <julie-nguyen@live.com>
2019-11-27 00:36:02 +0000

    First implementation of countLetters

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😊🤗 Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`😤😢 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const countLetters = function(aString) { 
+  let result = {};
+  for (const letter of aString) {
+    if (letter !== ' ') {
+      if (result[letter]) {
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }
+    }
+  }
+  return result;
+};
+
+console.log(countLetters('lighthouse in the house'));
