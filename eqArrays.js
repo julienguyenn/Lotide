b3d37aa5181f140a661f1d02426a95341e68e8e7
Julie Nguyen <julie-nguyen@live.com>
2019-11-28 20:34:21 +0000

    Revised to check nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,4 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`😊🤗 Assertion Passed: [${actual}] === [${expected}]`);
   } else {
@@ -6,21 +6,33 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(array1, array2) {
+const eqArrays = function (array1, array2) {
   if (array1 === undefined ||
     array2 === undefined ||
-    array1.length  !== array2.length) {
+    array1.length !== array2.length) {
     return false;
   }
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
+  if (Array.isArray(array1[i])) {
+    if (!Array.isArray(array2[i])) {
       return false;
+    } else {
+      let result = true && eqArrays(array1[i], array2[i]);
+      if (result === false) {
+        return false;
+      }
     }
+  } else if (array1[i] !== array2[i]) {
+    return false;
   }
-  return true;
+}
+return true;
 };
 
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
-console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false));
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true));
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false));
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); 
