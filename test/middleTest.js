157a6163e0cc8017a2996280b2b2577c366f157d
Julie Nguyen <julie-nguyen@live.com>
2019-12-02 16:57:08 +0000

    used mocha and chai to test code

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,16 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1,2]), []);
-assertArraysEqual(middle([1,2,3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2,3]);
+describe('#middle', () => {
+  it('return [] for [1]', () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it('return [2] for [1,2,3]', () => {
+    assert.deepEqual(middle([1,2,3]), [2]);
+  });
+
+  it('return [2,3] for [1,2,3,4]', () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
+});
