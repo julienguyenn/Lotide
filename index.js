93994a08940cd151be09f367bb2ee92fba8a5ce3
Julie Nguyen <julie-nguyen@live.com>
2019-12-02 17:26:46 +0000

    created index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  findKeyByValue: findKeyByValue,
+  findKey: findKey,
+  flatten: flatten,
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without
+};
